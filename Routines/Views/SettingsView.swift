1e7bcc284ebb0cc661599a084615e8a6211ec97b
Sam Clemente <sam@theclementes.com>
2024-07-31 14:39:58 -0400

    Added version and build numbers to settings page

--- Routines/Views/SettingsView.swift
+++ Routines/Views/SettingsView.swift
@@ -10,8 +10,13 @@ import SwiftUI
 
 struct SettingsView: View {
     @Binding var isPresented: Bool
+    let versionNumber = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
+    let buildNumber = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as! String
     
     var body: some View {
-        Text("SettingsView")
+        VStack {
+            Spacer()
+            Text("Version: \(versionNumber) (\(buildNumber))")
+        }
     }
 }
