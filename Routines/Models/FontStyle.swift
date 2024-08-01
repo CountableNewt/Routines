53586e7fb9c5e9925c794278bea3230c3cf34683
Sam Clemente <sam@theclementes.com>
2024-07-31 21:35:33 -0400

    Added content to Settings page

---
+++ Routines/Models/FontStyle.swift
@@ -0,0 +1,14 @@
+//
+//  FontStyle.swift
+//  Routines
+//
+//  Created by Sam Clemente on 7/31/24.
+//
+
+import SwiftUI
+
+struct FontStyle: Identifiable {
+    var id = UUID()
+    var name: String
+    var style: Font
+}
---
+++ Routines/Models/Symbol.swift
@@ -0,0 +1,16 @@
+//
+//  Symbol.swift
+//  Routines
+//
+//  Created by Sam Clemente on 7/31/24.
+//
+
+import SwiftUI
+
+struct Symbol: Identifiable {
+    var id = UUID()
+    var name: String
+    var file: String
+    var url: String
+    var color: Color
+}
